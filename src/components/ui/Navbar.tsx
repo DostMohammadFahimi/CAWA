e01736172b67f572cecfa1ccae037c6ad3de0622
Dost Mohammad Fahimi <dmfahimiiiii730@gmail.com>
2025-10-12 15:23:33 +0430

    install the shadcn liabrary use shadcn button.

---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import { Button } from "./button";
+import { Home, Sprout } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex items-center justify-between h-16 ">
+          {/* {logo} */}
+          <div className="flex items-center">
+            <Link
+              href="/"
+              className="text-xl font-bold text-primary font-mono tracking-wider"
+            >
+              🌱 Plantventory
+            </Link>
+          </div>
+          {/* {Navbar component} */}
+          <div className="hidden md:flex items-center space-x-4">
+            <Button variant="ghost" className="flex items-center gap-2" asChild>
+              <Link href="/plants">
+                <Sprout className="w-4 h-4" />
+                <span className="hidden lg:inline">Plants</span>
+              </Link>
+            </Button>
+
+            {/* {} */}
+            <Button variant="ghost" className="flex items-center gap-2" asChild>
+              <Link href="/">
+                <Home className="w-4 h-4" />
+                <span className="hidden lg:inline">Home</span>
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
